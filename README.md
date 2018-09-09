39caaa25af8086e966624e0befe34323447356cb
VivaGrendel <catalan.david@gmail.com>
2018-09-09 15:50:53 +0200

    Add MaterialUI, update README

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 Perfect Location
 ================
 
+
 ## Variables
 
 `GOOGLE_MAPS_API_KEY` (https://console.cloud.google.com/apis/credentials)
+
+
+## Appendix
+
+### Installation log
+
+```bash
+npm init -y
+npm i webpack --save-dev
+npm i webpack-cli --save-dev
+npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
+npm i react react-dom --save-dev
+npm i html-webpack-plugin html-loader --save-dev
+npm i webpack-dev-server --save-dev
+npm install @material-ui/core
+npm install typeface-roboto --save
+```
--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, {	Component } from 'react';
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return ( <div className = "App" >
+			<header className = "App-header" >
+				<h1 className = "App-title" > Welcome to React < /h1>
+			</header>
+			<Button variant = "contained" color = "primary" >Hello World</Button>
+			<p className = "App-intro" >
+				To get started, edit < code > src / App.js < /code> and save to reload. 
+			</p>
+		</div>
+		);
+	}
 }
 
 export default App;
